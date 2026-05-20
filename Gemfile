7d92d8a8dcd232953f74b7bbb413aefa6fdb82a0
Francois Bracq <francois.bracq@gmail.com>
2026-05-20 12:01:16 +0200

    Yelp

--- Gemfile
+++ Gemfile
@@ -55,5 +55,7 @@ group :development do
   # Use console on exceptions pages [https://github.com/rails/web-console]
   gem "web-console"
 end
+gem "simple_form"
+
 gem 'rspec-rails', group: [ :test ]
 gem 'rails-controller-testing', group: [ :test ]
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,15 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+Restaurant.destroy_all
+
+restaurants = [
+  { name: "Epicure", address: "75008 Paris", phone_number: "01 53 43 43 40", category: "french" },
+  { name: "Sushi Samba", address: "15 Rue de la Paix, 75002 Paris", phone_number: "01 42 61 05 09", category: "japanese" },
+  { name: "Osteria Ferrara", address: "7 Rue du Dahomey, 75011 Paris", phone_number: "01 43 71 67 69", category: "italian" },
+  { name: "Mirama", address: "17 Rue Saint-Jacques, 75005 Paris", phone_number: "01 43 54 71 77", category: "chinese" },
+  { name: "Le Chalet de la Forêt", address: "43 Drève de Lorraine, 1180 Bruxelles", phone_number: "+32 2 374 54 16", category: "belgian" },
+  { name: "Le Grand Véfour", address: "17 Rue de Beaujolais, 75001 Paris", phone_number: "01 42 96 56 27", category: "french" }
+]
+
+restaurants.each do |attrs|
+  r = Restaurant.create!(attrs)
+  puts "Created: #{r.name}"
+end
